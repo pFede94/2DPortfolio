2b200691057635507663a5bef51440f6e305cc92
Federico <f_palacio9494@outlook.com>
2024-05-23 01:33:19 -0300

    project updated to be deploy

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 //this config its to rectify some problems with kaboom and when its time to upload
 export default defineConfig({
     //used for the server to find the assets
-    base: "./",
+    base: "https://pfede94.github.io/2DPortfolio",
     //use this minify because of a bug in kaboom
     build: {
         minify: "terser"
